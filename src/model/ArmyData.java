14131d656951db410953e95c90a92c2cc2d650e5
Martin Mbiro <martinmbiro@gmail.com>
2022-02-25 11:54:53 +0300

    Updated ArmyData.java
    Pre-populated the ObservableList of Hero with a new Hero for debugging purposes

--- src/model/ArmyData.java
+++ src/model/ArmyData.java
@@ -6,4 +6,5 @@ import javafx.collections.ObservableList;
 public abstract class ArmyData {
 	//Pre-populate with one Hero object for debugging
 	public static ObservableList<Hero> mArmyData = FXCollections.observableArrayList(new Hero("Merlin","Mage",34,57,2));
+	//new Hero("Merlin","Mage",34,57,2)
 }
