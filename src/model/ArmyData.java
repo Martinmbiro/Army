a96082b5e17447d1e92c3c6286996e77ac117564
Martin Mbiro <martinmbiro@gmail.com>
2022-02-25 11:39:38 +0300

    Added comments to describe the class

--- src/model/ArmyData.java
+++ src/model/ArmyData.java
@@ -3,6 +3,9 @@ package model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/*This class holds the list of Heroes to be accessed from whatever class
+ * Made abstract so that it can never be instantiated, but rather accessed
+ * Referring to the MVC pattern, this would be the MODEL*/
 public abstract class ArmyData {
 	//Pre-populate with one Hero object for debugging
 	public static ObservableList<Hero> mArmyData = FXCollections.observableArrayList(new Hero("Merlin","Mage",34,57,2));
