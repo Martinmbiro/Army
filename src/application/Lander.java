825cedc230dc3e0f2553d753ae2ec212370546e6
Martin Mbiro <martinmbiro@gmail.com>
2022-02-25 11:43:54 +0300

    Updated Lander.java
    Added code to:
    - Make sure the window does not resize
    - Set window and taskbar icon
    - Set window title

--- src/application/Lander.java
+++ src/application/Lander.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
-
+/*The entry point into the program
+ * This class, together with "Lander.fxml" make for the VIEWS*/
 public class Lander extends Application {
 	@Override
 	public void start(Stage primaryStage) {
@@ -16,7 +18,15 @@ public class Lander extends Application {
 			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			//The window won't be resizable
+			primaryStage.setResizable(false);
+			//Set title for the window
+			primaryStage.setTitle("Create Your Army");
+			//Set Icon
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("spartan.png")));
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
