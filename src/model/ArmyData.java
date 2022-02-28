76fe7c4b18d5a967f426b58f97252983e72160be
Martin Mbiro <martinmbiro@gmail.com>
2022-02-28 09:55:12 +0300

    Added more Hero Objects to the ObservableList for debugging

--- src/model/ArmyData.java
+++ src/model/ArmyData.java
@@ -5,9 +5,13 @@ import javafx.collections.ObservableList;
 
 /*This class holds the list of Heroes to be accessed from whatever class
  * Made abstract so that it can never be instantiated, but rather accessed
- * Referring to the MVC pattern, this would be the MODEL*/
+ * Going by the MVC pattern, this would be the MODEL*/
 public abstract class ArmyData {
-	//Pre-populate with one Hero object for debugging
-	public static ObservableList<Hero> mArmyData = FXCollections.observableArrayList(new Hero("Merlin","Mage",34,57,2));
-	//new Hero("Merlin","Mage",34,57,2)
+	//Pre-populate with 5 Hero objects for debugging
+	public static ObservableList<Hero> mArmyData = FXCollections.observableArrayList(
+															new Hero("Merlin","Mage",96,3,50),
+															new Hero("Drew","Fighter",86,12,51),
+															new Hero("Asha","Unicorn",85,13,58),
+															new Hero("Bryson","Zombie",10,88,51),
+															new Hero("Morgana","Mage",90,2,82));
 }
